aa7d372022c841256db6d1f2780d61734a68c1f3
azawat7 <az4wat@gmail.com>
2025-02-08 23:45:03 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+      <>
+          <h1>Witam do naszej gry Kajak Racing</h1>
+          <span>Nic tu jeszcze nie ma bo czekamy na grafika/dev/nie wiem co</span>
+      </>
+  )
+}
+
+export default App
