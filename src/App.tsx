40b3d3963843ac046b5e76ea65bbf10f3ce34d32
azawat7 <az4wat@gmail.com>
2025-02-09 01:10:44 +0100

    style: prettier (4 spaces)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { KajakEngine } from '@kajaksolutions/kajakengine/src/index.ts'
+
 function App() {
-  return (
-      <>
-          <h1>Witam do naszej gry Kajak Racing</h1>
-          <span>Nic tu jeszcze nie ma bo czekamy na grafika/dev/nie wiem co</span>
-      </>
-  )
+    console.log(KajakEngine)
+    return (
+        <>
+            <h1>Witam do naszej gry Kajak Racing</h1>
+            <span>
+                Nic tu jeszcze nie ma bo czekamy na grafika/dev/nie wiem co
+            </span>
+        </>
+    )
 }
 
 export default App
